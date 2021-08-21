6696371743e3718189d31377663872572f4f7aa5
Anton Vlasov <nocsland@gmail.com>
2021-08-21 21:26:00 +0400

    add allure steps BasePage

--- page_objects/BasePage.py
+++ page_objects/BasePage.py
@@ -1,4 +1,5 @@
 import logging
+import allure
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -18,6 +19,7 @@ class BasePage:
         self.logger.info('browser was opened')
         return self
 
+    @allure.step('Проверка title')
     def verify_title(self, title):
         wait = WebDriverWait(self.browser, 5)
         wait.until(EC.title_is(title))
@@ -25,6 +27,7 @@ class BasePage:
         self.logger.info('title was verified')
         return self
 
+    @allure.step('Логин')
     def login(self, login, password):
         self.browser.find_element_by_name('username').clear()
         self.browser.find_element_by_name('username').send_keys(login)
@@ -34,28 +37,33 @@ class BasePage:
         self.logger.info("login completed")
         return self
 
+    @allure.step('Переключение окна')
     def switch_to_alert_and_ok(self):
         alert = self.browser.switch_to.alert
         alert.accept()
         self.logger.info('switch alert is ok')
         return self
 
+    @allure.step('Ожидание элемента')
     def wait_css_element(self, selector):
         wait = WebDriverWait(self.browser, 3)
         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
         self.logger.info('element is visible')
         return self
 
+    @allure.step('Переключение валюты')
     def click_switch_currency(self):
         self.browser.find_element_by_id('form-currency').click()
         self.logger.info('switch currency menu was called')
         return self
 
+    @allure.step('Выбор валюты')
     def click_to_currency(self, currency):
         self.browser.find_element_by_css_selector(f'button[name="{currency}"]').click()
         self.logger.info('currency was switched')
         return self
 
+    @allure.step('Открыть страницу административной панели')
     def open_admin(self):
         self.browser.get(self.url + '/admin/')
         return self
