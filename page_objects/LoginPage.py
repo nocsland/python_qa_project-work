d6206f9c6ba3917f530bcfc188e6ef1ee234f71c
Anton Vlasov <nocsland@gmail.com>
2021-08-21 22:29:56 +0400

    add allure steps loginPage

--- page_objects/LoginPage.py
+++ page_objects/LoginPage.py
@@ -1,29 +1,36 @@
+import allure
+
 from page_objects.BasePage import BasePage
 
 
 class LoginPage(BasePage):
     path = '/index.php?route=account/login'
 
+    @allure.step('Найти элемент "input-email"')
     def find_input_email(self):
         self.browser.find_element_by_id('input-email')
         self.logger.info('input_email was found')
         return self
 
+    @allure.step('Найти кнопку "Continue"')
     def find_continue_button(self):
         self.browser.find_element_by_link_text('Continue')
         self.logger.info('continue button was found')
         return self
 
+    @allure.step('Найти элемент "input-password"')
     def find_input_password(self):
         self.browser.find_element_by_xpath('//*[@id="input-password"]')
         self.logger.info('input-password was found')
         return self
 
+    @allure.step('Найти элемент "Forgotten Password"')
     def find_forgotten_password(self):
         self.browser.find_element_by_link_text('Forgotten Password')
         self.logger.info('Forgotten Password was found')
         return self
 
+    @allure.step('Найти кнопку "login"')
     def find_login_button(self):
         self.browser.find_element_by_xpath('//*[@id="content"]//form/input')
         self.logger.info('login button was found')
