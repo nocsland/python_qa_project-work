c00704148df4ae4cafb8f9536a871f4a2c42c98e
Anton Vlasov <nocsland@gmail.com>
2021-08-21 23:13:21 +0400

    add allure steps ProductCardPage

--- page_objects/ProductCardPage.py
+++ page_objects/ProductCardPage.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -8,6 +9,7 @@ from page_objects.BasePage import BasePage
 class ProductCardPage(BasePage):
     path = '/index.php?route=product/product&path=57&product_id=49'
 
+    @allure.step('Поиск элемента h1, ожидание загрузки плашек соцсетей')
     def find_h1_product(self):
         wait = WebDriverWait(self.browser, 3)
         wait.until(EC.visibility_of_element_located((By.XPATH, '//span[contains(.,"Tweet")]')))
@@ -15,21 +17,25 @@ class ProductCardPage(BasePage):
         self.logger.info("element was found")
         return self
 
+    @allure.step('Найти элемент "Add to cart"')
     def find_button_add_to_cart(self):
         self.browser.find_element_by_xpath('//*[@id="button-cart"]')
         self.logger.info("element was found")
         return self
 
+    @allure.step('Найти элемент "input-quantity"')
     def find_input_quantity(self):
         self.browser.find_element_by_id('input-quantity')
         self.logger.info("element was found")
         return self
 
+    @allure.step('Найти ссылку "Write a review"')
     def find_write_a_review(self):
         self.browser.find_element_by_link_text('Write a review')
         self.logger.info("element was found")
         return self
 
+    @allure.step('Найти вкладку "Description"')
     def find_tab_description(self):
         self.browser.find_element_by_id('tab-description')
         self.logger.info("element was found")
